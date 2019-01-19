99a955c4dbdce311cf5bba12c6e99e4cc47f9738
humeyraaydiner <40187177+humeyraaydiner@users.noreply.github.com>
2019-01-19 13:19:36 -0500

    I added one more line of code

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
     public static  double multply (double num1,double num2){
         return num1 * num2;
 }
+    
+        public static  double division (double num1,double num2){
+        return num1 / num2;
+}
 }
